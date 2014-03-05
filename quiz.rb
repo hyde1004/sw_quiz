ab92718bc39e492e49d67dad7b7ca96f2f05b134
이정구 <junggu.lee@lge.com>
2014-03-05 16:25:38 +0900

    static

--- quiz.rb
+++ quiz.rb
@@ -11,6 +11,26 @@ get '/test' do
 	'Test'
 end
 
-get '/:id' do
-	erb :question
+# get '/:id' do
+# 	erb :question
+# end
+
+get '/1' do
+	'<p>3+3은 얼마인가요?</p>
+	<ol>
+	<li>3</li>
+	<li>4</li>
+	<li>5</li>
+	<li>6</li>
+	</ol>'
+end
+
+get '/2' do
+	'<p>음악의 아버지는?</p>
+	<ol>
+	<li>모짜르트</li>
+	<li>바하</li>
+	<li>헨델</li>
+	<li>비발디</li>
+	</ol>'	
 end
