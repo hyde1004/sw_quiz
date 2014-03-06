6efdedde5a865015a099461c9e82241e6ced6b64
Junggu Lee <hyde1117@naver.com>
2014-03-07 00:02:16 +0900

    문제를 정적에서 동적으로 변환

--- quiz.rb
+++ quiz.rb
@@ -2,6 +2,10 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'yaml'
+
+questions = YAML.load_file("questions.yml")
+#puts @quesetions[0]['name']
 
 get '/' do
 	erb :home
@@ -11,18 +15,24 @@ get '/test' do
 	'Test'
 end
 
-# get '/:id' do
-# 	erb :question
+get '/:id' do
+	@id = params[:id].to_i - 1
+	@desc = questions[@id]['desc']
+	@list1 = questions[@id]['list1']
+	@list2 = questions[@id]['list2']
+	@list3 = questions[@id]['list3']
+	@list4 = questions[@id]['list4']
+	erb :question
+end
+
+# get '/1' do
+# 	erb :'1'
 # end
 
-get '/1' do
-	erb :'1'
-end
+# get '/2' do
+# 	erb :'2'
+# end
 
-get '/2' do
-	erb :'2'
-end
-
-get '/3' do
-	erb :'3'
-end
+# get '/3' do
+# 	erb :'3'
+# end
